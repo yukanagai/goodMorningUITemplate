16d4df806c6917d040bf78625ce8591d51944c99
Yuka Nagai <yukanagai@gmail.com>
2017-04-03 01:46:33 -0400

    updated broken favicon filepath + rewrote scroll with vanilla js

--- src/js/index.js
+++ src/js/index.js
@@ -1,15 +1,14 @@
-$(function() {
- $(window).scroll(function () {
-    if ($(this).scrollTop() > 50) {
-       $('body').addClass('changeColor');
-       $('header').addClass('changeHeader');
+(function() {
+  window.onscroll = function() {
+    if (window.document.body.scrollTop > 50) {
+      document.body.classList.add("changeColor");
+      document.querySelector('header').classList.add("changeHeader");
+    } else {
+      document.body.classList.remove("changeColor");
+      document.querySelector('header').classList.remove("changeHeader");
     }
-    if ($(this).scrollTop() < 50) {
-       $('body').removeClass('changeColor');
-       $('header').removeClass('changeHeader');
-    }
- });
-});
+  };
+})();
 
 document.querySelector('form').addEventListener("submit", function(e) {
 	e.preventDefault();
